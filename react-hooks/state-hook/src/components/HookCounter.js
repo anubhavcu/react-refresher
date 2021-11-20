dc8adf7a2fc904820bbb5cd666410d8eb8d74d72
anubhavcu <anubhav342y@gmail.com>
2021-11-21 01:04:15 +0530

    state hook imp points

--- react-hooks/state-hook/src/components/HookCounter.js
+++ react-hooks/state-hook/src/components/HookCounter.js
@@ -55,3 +55,12 @@ export default HookCounter;
  *
  * better to use approach 2, as we depend on prev state for our next state update
  */
+/**
+ * useState hook
+ * - let's you add state to functional component , by calling useState we are creating a single piece of state
+ * - In classes, state is always an object, while with useState hook, state doesn't need to be an object
+ * - useState hook returns an array with 2 elements, first is the current state of the element, and second is state setter function
+ * - we can pass a cb function to state setter method if our state value depend on prev values. This cb function will take prev state as arguments
+ * - When dealing with object and arrays, always make sure to spread your state variables (merge with prev state)
+ * - because with using hooks, state doesn't get merged, but whole new state is created, whereas in class component state was merged
+ */
